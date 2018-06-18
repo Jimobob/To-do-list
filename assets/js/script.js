f02308f2175ba84f0730d093f2842b0445d04000
Jimmy <JimmyCao227@yahoo.com>
2018-06-17 20:43:34 -0400

    Added extra functionality. Checkboxes can be toggled to cross out items in list. Edit and delete still needs to be implemented.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
+var id = 0;
+
 $("input").on("keypress", function(event){
 	if(event.which == 13){
+		id++;
 		var value = $("input").val();
-		$("#list").append("<p>" + value + "</p");
-		console.log(value);
+
+		var $div = $("<div>").append(
+			$("<i>", {class: "fas fa-trash-alt fa-sm"}),
+			$("<i>", {class: "far fa-edit fa-sm"}),
+			$("<input>", {type: "checkbox", class: "checkbox", id: "cb" + id}, name="checkbox"),
+			$('<label>', {for: "cb" + id, text: value})
+		).appendTo("#list");
+
+
+		console.log($div);
+
 		$("input").val('');
 	}
+})
+
+
+$(document).on('change', '[type=checkbox]', function(){
+	var id = $(this).attr("id");
+	if($(this).is(":checked")){
+		$("label[for='" + id + "']").css("text-decoration", "line-through");
+	}
+	else{
+		$("label[for='" + id + "']").css("text-decoration", "none");
+	}
+})
+
+
+$("p").on("click", function(){
+
 })
