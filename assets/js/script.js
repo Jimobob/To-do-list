8a8c08f586e6c476b1ef20ba08700c9f3f464531
Jimmy <JimmyCao227@yahoo.com>
2018-06-18 00:00:56 -0400

    Added functionality to all widgets. Delete and edits work properly and checkboxes cross off text. Cleaned up contents in the files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,9 @@ $("input").on("keypress", function(event){
 			$('<label>', {for: "cb" + id, text: value})
 		).appendTo("#list");
 
-
-		console.log($div);
-
 		$("input").val('');
 	}
-})
+});
 
 
 $(document).on('change', '[type=checkbox]', function(){
@@ -28,9 +25,34 @@ $(document).on('change', '[type=checkbox]', function(){
 	else{
 		$("label[for='" + id + "']").css("text-decoration", "none");
 	}
-})
+});
+
+$(document).on("click", ".fa-trash-alt", function(){
+	$(this).parent().remove();
+});
+
+$(document).on("click", ".fa-edit", function(){
+	var oldVal = $(this).parent().children("label").text();
+
+	var $text = "<input type='text' name='text' value='" + oldVal + "'>";
+
+	$(this).parent().children("label").replaceWith($text);
+
+	$("input[name='text']").css({
+		"margin-left" : "15px",
+		"outline" : "none"
+	});
+
+	$("input[name='text']").focus();
+
+	$("input[name='text']").on("keypress", function(event){
+		if(event.which == 13){
+			var value = $("input[name='text']").val();
 
 
-$("p").on("click", function(){
+			var $newText = "<label for='checkbox'>" + value + "</label>";
+			$("input[name='text']").replaceWith($newText);
+		}
+	})
 
 })
